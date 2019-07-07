b70d4498219f46374efde5c36c2d0813eb70bf9b
David Gesrob <david81br@gmail.com>
2019-07-07 20:14:35 -0300

    Fixed Css and Fontawesome

--- src/main/java/com/pegasus/justicehub/auth/controller/UserController.java
+++ src/main/java/com/pegasus/justicehub/auth/controller/UserController.java
@@ -42,7 +42,7 @@ public class UserController {
 
         securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
 
-        return "redirect:/welcome";
+        return "redirect:/";
     }
 
     @GetMapping("/login")
